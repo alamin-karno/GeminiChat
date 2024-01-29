ddbdad768193ecafb13d81e7c8a24797b5a69098
alamin-karno <amin15-1951@diu.edu.bd>
2024-01-29 15:38:03 +0600

    GeminiChat: Initialization

---
+++ lib/features/chat/chat.dart
@@ -0,0 +1,1 @@
+export 'presentation/ui/chat_screen.dart';
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:gemini_chat/features/chat/chat.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GeminiChat',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const ChatScreen(),
+    );
+  }
+}
